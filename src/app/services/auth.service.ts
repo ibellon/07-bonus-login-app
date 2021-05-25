e9143d0d8a9fefbdb31d225366ebc74fa5c0c492
ibellon <isidro.bellon@gmail.com>
2021-05-25 16:10:35 +0200

    Guardar Token en el LocalStorage

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UsuarioModel } from '../models/usuario.model';
 
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,9 +11,10 @@ export class AuthService {
 
   private URL = "https://identitytoolkit.googleapis.com/v1/accounts:";
   private API_KEY = "AIzaSyCDP9ZqX7r5CU_1xBm8XFM3w1B6tGWdhPI";
+  private userToken: string;
 
   constructor(private http: HttpClient) { 
-
+    this.leerToken();
   }
 
   logout() {
@@ -28,7 +31,13 @@ export class AuthService {
 
     return this.http.post(
       this.URL+"signInWithPassword?key="+this.API_KEY, 
-      authData);
+      authData).pipe(
+        map( resp => {
+          console.log("Entró en el map del RXJS");
+          this.guardarToken(resp['idToken'])
+          return resp;
+        })
+      );
 
   }
 
@@ -42,8 +51,33 @@ export class AuthService {
 
     return this.http.post(
       this.URL+"signUp?key="+this.API_KEY, 
-      authData);
+      authData).pipe(
+        map( resp => {
+          console.log("Entró en el map del RXJS");
+          this.guardarToken(resp['idToken'])
+          return resp;
+        })
+      );
 
   }
   
+  private guardarToken( idToken: string ) {
+    
+    this.userToken = idToken;
+    localStorage.setItem('token', idToken);
+
+  }
+
+  leerToken() {
+    
+    if(localStorage.getItem('token')) {
+      this.userToken = localStorage.getItem('token');
+    }
+    else {
+      this.userToken = '';
+    }
+
+    return this.userToken;
+  }
+
 }
